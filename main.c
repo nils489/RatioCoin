7b316442640f9fec8b88d98ed054bb853c6caa6f
LNelson89 <LNelson@gmx.de>
2017-10-03 22:45:46 +0200

    removed test code from main.c

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include "blockchain.h"
 #include "proofofwork.h"
 #include "crypto_ec.h"
+#include "hash_utils.h"
+#include "test.h"
 
 int mine() {
 	int retBC, retPW;
@@ -12,24 +15,12 @@ int mine() {
 	return retBC || retPW;
 }
 
-int create_keypair() {
-	const uint8_t dummy_bytes=5;
-	EC_KEY *key;
-	key = create_rc_ec_keypair(&dummy_bytes);
-	if(key==NULL) {
-		printf("WARNING! No key generated!\n");
-	}
-	return 0;
-}
-
-
 int main() {
 	int retM;
 
 	printf("Welcome to the RatioCoin client!\n");
 
-	printf("Creating keypair ...\n");
-	create_keypair();
+	run_tests();
 
 	retM = mine();
 	if(!retM) {
